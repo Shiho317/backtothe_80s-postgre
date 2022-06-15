8003d6384ec8dbe5937ed5aac03a821fde8784ef
Shiho Nagano <ex3jsb.geneg@gmail.com>
2022-06-15 15:11:52 -0700

    deploy pern app

--- services/db.js
+++ services/db.js
@@ -3,12 +3,13 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+const devConfig = `postgresql://${process.env.POOL_USER}:${process.env.POOL_PASSWORD}@${process.env.POOL_HOST}:${process.env.POOL_PORT}/${process.env.POOL_DATABASE}`;
+
+const proConfig = process.env.DATABASE_URL; //heroku addons
+
 const pool = new Pool({
-  user: process.env.POOL_USER,
-  password: process.env.POOL_PASSWORD,
-  host: process.env.POOL_HOST,
-  port: process.env.POOL_PORT,
-  database: process.env.POOL_DATABASE
+  connectionString:
+    process.env.NODE_ENV === "production" ? proConfig : devConfig,
 });
 
 module.exports = pool;
