c0bbfa484f14f8311d99d1ae5b6d251bb1c471ae
Shiho Nagano <ex3jsb.geneg@gmail.com>
2022-06-15 18:08:43 -0700

    changed ssl

--- services/db.js
+++ services/db.js
@@ -5,11 +5,19 @@ dotenv.config();
 
 const devConfig = `postgresql://${process.env.POOL_USER}:${process.env.POOL_PASSWORD}@${process.env.POOL_HOST}:${process.env.POOL_PORT}/${process.env.POOL_DATABASE}`;
 
+// const pool = new Pool({
+//   user: process.env.POOL_USER,
+//   password: process.env.POOL_PASSWORD,
+//   host: process.env.POOL_HOST,
+//   port: process.env.POOL_PORT,
+//   database: process.env.POOL_DATABASE
+// })
+
 const proConfig = process.env.DATABASE_URL; //heroku addons
 
 const pool = new Pool({
-  connectionString:
-    process.env.NODE_ENV === "production" ? proConfig : devConfig,
+  connectionString: process.env.NODE_ENV === "production" ? proConfig : devConfig,
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
